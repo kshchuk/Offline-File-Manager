ce6732636c2a5f13be7a8ed1261d71e446b5f42f
kshchuk <kshchuk@gmail.com>
2022-11-01 17:25:59 +0200

    Added and stylesheets

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile styleFile("res/css/Combinear.qss");
+    styleFile.open(QFile::ReadOnly);
+    QString style(styleFile.readAll());
+    a.setStyleSheet(style);
     OfflineFileManager w;
     w.show();
     return a.exec();
